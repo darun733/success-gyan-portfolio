f022ed75569160ab15d106ee5b9aaa7d8d317cc2
darun733 <darun733@gmail.com>
2025-12-25 12:40:52 +0530

    fix: fixed alignment issue3

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,17 +3,24 @@ export default {
     darkMode: 'class',
     content: ["./src/**/*.{html,ts}"],
     theme: {
-        extend: {
-              colors: {
-                brand: { primary:"#2F6FED" },
-              },
-              fontFamily: {
-                poppins: ['Poppins'],
-              },
-               boxShadow: {
-                soft: "0px 0px 22.2px 0px rgba(0, 0, 0, 0.11)",
-              },
-        },
+       screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      extend: {
+            colors: {
+              brand: { primary:"#2F6FED" },
+            },
+            fontFamily: {
+              poppins: ['Poppins'],
+            },
+              boxShadow: {
+              soft: "0px 0px 22.2px 0px rgba(0, 0, 0, 0.11)",
+            },
+      },
     },
     plugins: [],
 };
